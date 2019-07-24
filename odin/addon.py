bab6c851d8ae34f3ea8c75c4e98f398ec37924de
Naglis Jonaitis <naglis@mailbox.org>
2019-07-24 20:35:10 +0300

    Add `Addon.qweb_files`

--- odin/addon.py
+++ odin/addon.py
@@ -1,3 +1,4 @@
+import ast
 import dataclasses
 import pathlib
 import typing
@@ -34,3 +35,20 @@ class Addon(AddonPath):
     @property
     def demo_files(self):
         return [self.path / fn for fn in (self.manifest.get("demo") or [])]
+
+    @property
+    def qweb_files(self):
+        files = []
+        for fn in self.manifest.get("qweb") or []:
+            fn_path = self.path / fn
+            if "*" in fn_path.stem:
+                files.extend(fn_path.parent.glob(fn_path.name))
+            else:
+                files.append(fn_path)
+        return files
+
+
+def parse_manifest(addon_path: AddonPath):
+    # FIXME: Check for manifest file size.
+    with addon_path.manifest_path.open(mode="r") as f:
+        return ast.literal_eval(f.read())
