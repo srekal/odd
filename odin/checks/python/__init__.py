5769fbc7264f8ef677dad7eaa8547d3334627419
Naglis Jonaitis <naglis@mailbox.org>
2019-08-04 15:59:35 +0300

    manifest_keys: add support for manifest key line numbers
    Closes #39

--- odin/checks/python/__init__.py
+++ odin/checks/python/__init__.py
@@ -1,6 +1,7 @@
 from .field_attrs import FieldAttrs
+from .manifest_keys import ManifestKeys
 from .route_kwargs import RouteKwargs
 from .track_visibility_always import TrackVisibilityAlways
 
 
-__all__ = ["FieldAttrs", "RouteKwargs", "TrackVisibilityAlways"]
+__all__ = ["FieldAttrs", "ManifestKeys", "RouteKwargs", "TrackVisibilityAlways"]
---
+++ tests/data/manifest_keys/manifest_keys_unknown_keys_complex/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Manifest Keys Test (unknown keys)",
+    "data": [
+    ],
+    "depends": [
+        "base",
+    ],
+    'external_dependencies': {
+        'python': [
+            'lxml',
+        ],
+    },
+    "foo": "Bar",
+}
