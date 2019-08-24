b460b6214cd3109a4b33d52ccae27c382690650c
Naglis Jonaitis <naglis@mailbox.org>
2019-08-24 12:28:30 +0300

    base: remove redundant comment

--- odd/check/base.py
+++ odd/check/base.py
@@ -5,18 +5,3 @@ import typing
 class Check(abc.ABC):
     _emits: typing.Set[str] = set()
     _handles: typing.Set[str] = set()
-
-    """
-    def on_before(self, addon: Addon):
-
-    def on_path(self, addon: Addon, path: pathlib.Path):
-        '''Receives paths (files or directories).'''
-
-    def on_python_module(
-        self, addon: Addon, filename: pathlib.Path, module: parso.python.tree.Module
-    ):
-
-    def on_xml_tree(self, addon: Addon, filename: pathlib.Path, tree):
-
-    def on_after(self, addon: Addon):
-    """
